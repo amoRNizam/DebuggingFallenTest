072084872f0c14b712b0532cf97e073b2e7a3e7d
jet_kinyak <123>
2019-11-14 22:43:55 +0300

    ***

---
+++ src/main/java/JListExample.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.math.BigDecimal;
+
+public class JListExample {
+    JFrame frame = new JFrame("Storage");
+    JList<Product> list = new JList<>();
+    DefaultListModel<Product> model = new DefaultListModel<>();
+    JLabel label = new JLabel();
+    JPanel panel = new JPanel();
+    JSplitPane splitPane = new JSplitPane();
+    public JListExample() {
+        list.setModel(model);
+        model.addElement(new Product("Item1", new BigDecimal("49.00")));
+        model.addElement(new Product("Item2", new BigDecimal("150")));
+        model.addElement(new Product("Item3", new BigDecimal("54.5")));
+        model.addElement(new Product("Item4", new BigDecimal("120.00")));
+        list.getSelectionModel().addListSelectionListener(e -> {
+            Product p = list.getSelectedValue();
+            label.setText(p.getName() + " price is = " + p.getPrice().toPlainString());
+        });
+        splitPane.setLeftComponent(new JScrollPane(list));
+        panel.add(label);
+        splitPane.setRightComponent(panel);
+        splitPane.setResizeWeight(0.5); //used to set the way it splits the left and right component
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(splitPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setSize(500,300);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(JListExample::new);
+    }
+    private class Product {
+        String name;
+        BigDecimal price;
+        public Product(String name, BigDecimal price) {
+            this.name = name;
+            this.price = price;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public BigDecimal getPrice() {
+            return price;
+        }
+        public void setPrice(BigDecimal price) {
+            this.price = price;
+        }
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
---
+++ src/main/java/ListModelTest.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.util.Vector;
+
+public class ListModelTest extends JFrame
+{
+    // Модель списка
+    private DefaultListModel<String> dlm = new DefaultListModel<String>();
+
+    private final String[] data1 = { "Чай" ,"Кофе"  ,"Минеральная","Морс"};
+    private final String[] data2 = { "Ясли","Детсад", "Школа"     , "Институт",
+            "Университет"};
+
+    public ListModelTest()
+    {
+        super("Пример со списком JList");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Создание панели
+        JPanel contents = new JPanel();
+
+        // Наполнение модели данными
+        for (String string : data2) {
+            dlm.add(0, string);
+        }
+        // Создание кнопки
+        JButton add = new JButton("Добавить");
+        add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dlm.add(dlm.getSize(), "-- Новая запись --");
+                validate();
+            }
+        });
+        JList<String> list1 = new JList<String>(data1);
+        JList<String> list2 = new JList<String>(dlm);
+
+        // Вектор данных
+        Vector<String> big = new Vector<String>();
+        for (int i=0; i < 15; i++) {
+            big.add("~ " + i + ". ~");
+        }
+        JList<String> bigList = new JList<String>(big);
+        bigList.setPrototypeCellValue("12345");
+
+        // Размещение компонентов в панели
+        contents.add(add);
+        contents.add(new JScrollPane(list1));
+//        contents.add(new JScrollPane(list2));
+//        contents.add(new JScrollPane(bigList));
+
+        setContentPane(contents);
+        // Вывод окна
+        setSize(400, 200);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new ListModelTest();
+    }
+}
